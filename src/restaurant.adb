512b885f25c8e0ad00c4082c80c81cf51a7e28e7
Ruben Bustos <ruben_b_r_@hotmail.com>
2018-06-16 21:51:41 +0200

    basic usage of array_enum

--- src/restaurant.adb
+++ src/restaurant.adb
@@ -1,6 +1,26 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with darray_enum;
+
 procedure Restaurant is
 
+   type FOOD_CLASS is (STARTER, FIRST);
+
+   package FoodClassArray is new darray_enum(item => Character, key => FOOD_CLASS); use FoodClassArray;
+
+   my_array: array_enum;
+
+   char1, char2: Character;
+
 begin
-   --  Insert code here.
-   null;
+
+   --a_empty(my_array, 'X');
+   a_put(my_array, STARTER, 'S');
+   a_put(my_array, FIRST, 'O');
+
+   a_get(my_array, STARTER, char1);
+   a_get(my_array, FIRST, char2);
+
+   Put(char1);
+   Put(char2);
+
 end Restaurant;
