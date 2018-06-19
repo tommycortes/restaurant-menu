3793374047926efb774944be9db8b369f4fd51a7
tommycortes <allmy42@gmail.com>
2018-06-19 20:43:37 +0200

    modified dtrie structure to discrimine between nodes

--- src/dtrie.adb
+++ src/dtrie.adb
@@ -6,8 +6,8 @@ package body dtrie is
    
    procedure t_empty(t: out trie) is
    begin
-      t.root:= new cell;
-      t.root.all:= (others => null);
+      t.root:= new cell(INTERIOR);
+      t.root.ti:= (others => null);
    end t_empty;
    
    function t_exists(t: in trie; k: in key) return boolean is
@@ -17,28 +17,31 @@ package body dtrie is
       
    begin
       p:= t.root; i:= i0; c:= k(i);
-      while c /= mk and p.all(c) /= null loop
-         p:= p.all(c); i:= i+1; c:= k(i);
+      while c /= mk and p.ti(c) /= null loop
+         p:= p.ti(c); i:= i+1; c:= k(i);
       end loop;
       
-      return c = mk and p.all(mk) = p;
+      return c = mk and p.ti(mk) = p;
    end t_exists;
    
-   procedure t_put(t: in out trie; k: in key) is
+   procedure t_put(t: in out trie; k: in key; x: in elem) is
       p, r: pcell;
       i: key_index;
       c: key_component;
    begin
       p:= t.root; i:= i0; c:= k(i);
       while c/= mk loop
-         if p.all(c) = null then
-            r:= new cell;
-            r.all:= (others => null);
-            p.all(c):= r;
+         if p.ti(c) = null then
+            r:= new cell(INTERIOR);
+            r.ti:= (others => null);
+            p.ti(c):= r;
          end if;
-         p:= p.all(c); i:= i+1; c:= k(i);
+         p:= p.ti(c); i:= i+1; c:= k(i);
       end loop;
-      p.all(mk):= p;
+      r:= new cell(LEAF);
+      r.tc:= x;
+      p.ti(mk):= r;
+      
    exception
       when Storage_Error => raise overflow;
    end t_put;
@@ -53,23 +56,23 @@ package body dtrie is
       begin
          c:= mk; nd:= 0;
          while c/= lastc and nd<2 loop
-            if p.all(c)/= null then nd:= nd+1; end if;
+            if p.ti(c)/= null then nd:= nd+1; end if;
             c:= key_component'Succ(c);
          end loop;
-         if p.all(c)/= null then nd:= nd +1; end if;
+         if p.ti(c)/= null then nd:= nd +1; end if;
          return nd<2;
       end only_desc;
       
    begin
       p:= t.root; i:= i0; c:= k(i); r:= p; cr:= c;
-      while c/= mk and p.all(c)/= null loop
+      while c/= mk and p.ti(c)/= null loop
          if not only_desc(p) then r:= p; cr:= c; end if;
-         p:= p.all(c); i:= i+1; c:= k(i);
+         p:= p.ti(c); i:= i+1; c:= k(i);
       end loop;
-      if c = mk and p.all(mk) = p then
+      if c = mk and p.ti(mk) = p then
          if only_desc(p) then
-            r.all(cr):= null;
-         else p.all(mk):= null;
+            r.ti(cr):= null;
+         else p.ti(mk):= null;
          end if;
       end if;
    end t_delete;
--- src/dtrie.ads
+++ src/dtrie.ads
@@ -1,16 +1,17 @@
 generic 
+   type elem is private;
    type key_component is (<>);
    type key_index is range <>;
    type key is array(key_index) of key_component;
    
 package dtrie is   
    
-   type trie is limited private;
+   type trie is private;
    
    overflow: exception;
    
    procedure t_empty(t: out trie);
-   procedure t_put(t: in out trie; k: in key);
+   procedure t_put(t: in out trie; k: in key; x: in elem);
    function t_exists(t: in trie; k: in key)return boolean;
    procedure t_delete(t: in out trie; k: in key);
    function t_is_empty(t: in trie) return boolean;
@@ -18,7 +19,17 @@ package dtrie is
 private
    type cell;
    type pcell is access cell;
-   type cell is array(key_component) of pcell;
+   type TCELL is (INTERIOR, LEAF);
+   type apcell is array(key_component) of pcell;
+   type cell (t: TCELL) is record
+      case t is 
+         when INTERIOR => 
+            ti: apcell;
+         when LEAF => 
+            tc: elem;
+      end case;
+   end record;
+   
    type trie is record
       root: pcell;
    end record;
